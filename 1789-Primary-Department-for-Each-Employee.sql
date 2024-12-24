020d16de4f278f7bc1701862d0fddb2ae6c360bd
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-12-24 20:03:51 +0200

    [1789] [Time Beats: 29.85%] - LeetPush

---
+++ 1789-Primary-Department-for-Each-Employee.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+-- SELECT employee_id,department_id FROM Employee  WHERE primary_flag = 'Y' 
+-- GROUP BY employee_id,department_id
+-- UNION
+SELECT employee_id, department_id FROM Employee WHERE employee_id IN (
+SELECT employee_id FROM Employee
+GROUP BY employee_id HAVING COUNT(department_id) =1) OR primary_flag = 'Y'
