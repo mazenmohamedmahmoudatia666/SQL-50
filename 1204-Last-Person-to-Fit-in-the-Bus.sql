69130ea2250a83252f7965722fca29bfde5ec674
MazenBerg <75747261+mazenmohamedmahmoudatia666@users.noreply.github.com>
2024-12-25 15:30:36 +0200

    [1204] [Time Beats: 8.23%] - LeetPush

--- 1204-Last-Person-to-Fit-in-the-Bus.sql
+++ 1204-Last-Person-to-Fit-in-the-Bus.sql
@@ -1,12 +1,8 @@
-WITH CumulativeWeight AS (
-    SELECT 
-        person_name,
-        weight,
-        SUM(weight) OVER (ORDER BY turn) AS cumulative_weight
-    FROM Queue
-)
-SELECT person_name
-FROM CumulativeWeight
-WHERE cumulative_weight <= 1000
-ORDER BY cumulative_weight DESC
-LIMIT 1;
+SELECT person_name FROM (
+    SELECT person_name, SUM(weight) OVER (
+        ORDER BY turn
+    ) s FROM Queue 
+    ORDER BY turn DESC
+) 
+WHERE s <= 1000
+LIMIT 1
